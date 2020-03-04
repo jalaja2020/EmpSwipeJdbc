9924c66aafbf365f12440b0b1fdbcb5c9bf04194
jalaja kumari <jalaja.kumari.n@gmail.com>
2020-03-04 05:27:13 -0800

    employee swipe jdbc

---
+++ src/main/java/com/hcl/controller/EmployeeController.java
@@ -0,0 +1,42 @@
+package com.hcl.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.model.Employee;
+import com.hcl.service.impl.EmployeeServiceImpl;
+
+@RestController
+public class EmployeeController {
+
+	@Autowired
+	EmployeeServiceImpl empService;
+
+	@RequestMapping(value = "/getAllEmployees", method = RequestMethod.GET)
+	public void getAllEmployees(Model model)
+	{
+		empService.getAllEmployees();
+	}
+
+	@RequestMapping(value = "/getAllEmployees/admin/{id}", method = RequestMethod.GET)
+	public List<Employee> getAdminEmployeeById(@RequestParam String id)
+	{
+		return empService.getEmployeeById(id);
+	}
+
+
+	@RequestMapping(value = "/getAllEmployees/admin/location/{location}", method= RequestMethod.GET) 
+	public List<Employee> getUserEmployeeByLocation(@RequestParam String location) { return
+			empService.getEmployeeByLocation(location); }
+
+	@RequestMapping(value = "/getAllEmployees/admin/date/{date}", method =RequestMethod.GET)
+	public List<Employee> getUserEmployeeByDate(@RequestParam
+					String date) { return empService.getEmployeeByDate(date); }
+
+}
